4a5eec1d35f344075b102506da78a0a1971a9d66
beboopr <linnets-router.09@icloud.com>
2022-11-15 14:36:08 -0500

    added more lang

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
-import { useState, createContext } from 'react';
-import Details from './component/Details';
+import { useState, createContext } from 'react'
+import Details from './components/Details'
 import English from './lang/en.json'
 import Spanish from './lang/es.json'
-import './App.css';
+import French from './lang/fr.json'
+import './App.css'
 
 const locale = navigator.language || 'en';
-const _lang = (locale==='es') ? Spanish : English;
 
+const _lang = (locale === 'es')
+  ? Spanish
+  : (locale === 'fr')
+    ? French
+    : English;
 
-export const languageContext = createContext()
+export const LanguageContext = createContext()
 
 function App() {
   const [lang, setLang] = useState(_lang)
   return (
-    <languageContext.Provider value={{ lang }}>
-    <div className="App">
-      <header className="App-header">
-       <div>
-        <button onClick={() => setLang(English)}>EN</button>
-        <button onClick={() => setLang(Spanish)}>ES</button>
-        </div>
-        <h1>{lang['app.header']}</h1>
-        <p>{lang["app.subhead"]}</p>        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Details/>
-      </header>
-    </div>
-        </languageContext.Provider>
+    <LanguageContext.Provider value={{ lang }}>
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <button onClick={() => setLang(English)}>EN</button>
+            <button onClick={() => setLang(Spanish)}>ES</button>
+            <button onClick={() => setLang(French)}>FR</button>
+          </div>
+          <h1>{lang["app.header"]}</h1>
+          <p>{lang["app.subhead"]}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {lang["app.cta"]}
+          </a>
+          <Details />
+        </header>
+      </div>
+    </LanguageContext.Provider>
   );
 }
 
